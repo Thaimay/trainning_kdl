fa467f13541c6ea06bfb72dd19c1f66395919947
QuyenLS <danquyen6596@gmail.com>
2019-01-18 17:27:34 +0700

    Add project use Spring Boot + Mybatis + Thymeleaf + Postgresql.

---
+++ SpringBootAndPostgre/src/main/java/com/saisystem/springboot/mapper/ProductMapper.java
@@ -0,0 +1,9 @@
+package com.saisystem.springboot.mapper;
+
+
+import javax.swing.tree.RowMapper;
+import com.saisystem.springboot.model.Product;
+
+public class ProductMapper implements RowMapper {
+
+}
